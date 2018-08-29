a1b2063605299ea25b0256f3f284ceff2656684e
Fernando Giardina <fgiardina@outlook.com>
2018-08-29 15:17:29 -0300

    spaces

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::group(['namespace'=>'fernandogiardina\afip\Http\Controllers'], function() {
+
     Route::get('/afip/wsa4/{id}', 'AfipController@CallWSA4')->name('afip.wsa4');
-    Route::get('/afip/token/{service}', 'AfipController@getToken')->name('afip.token');    
+    Route::get('/afip/token/{service}', 'AfipController@getToken')->name('afip.token');
+
 });
