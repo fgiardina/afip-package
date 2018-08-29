a1925bc46f6f31f3cc7a68f9b7de2aee5933111e
fgiardina <fgiardina@outlook.com>
2018-08-29 14:35:24 -0300

    Update routes.php

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,9 +1,5 @@
 <?php
 
-Route::get('hola',function(){
-    return 'holahola';
-});
-
 Route::group(['namespace'=>'fernandogiardina\afip\Http\Controllers'], function() {
     Route::get('/afip/wsa4/{id}', 'AfipController@CallWSA4')->name('afip.wsa4');
     Route::get('/afip/token/{service}', 'AfipController@getToken')->name('afip.token');    
